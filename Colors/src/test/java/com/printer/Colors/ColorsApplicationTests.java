d30c1b0d752f8cf7a3d77eddec45029fdba23fbd
vinayydv3695 <vinayydv343@gmail.com>
2025-02-18 20:49:26 +0530

    chore: getting udes to annotations and config classes

---
+++ Colors/src/test/java/com/printer/Colors/ColorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.printer.Colors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
